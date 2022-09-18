bbdf9262c8eb5eb9b1f840bd1746af997fbd245f
andrey <elefant86@bk.ru>
2022-09-18 21:05:56 +0300

    migration fixed

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 https://i.imgur.com/qGvFUYB.png
+DB_NAME = todolist
+DB_USER = runov
+DB_PASS = 123
+DB_URL = 127.0.0.1
+PORT = 3000
--- server/db/models/todo.js
+++ server/db/models/todo.js
@@ -1,26 +1,27 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+'use strict'
+const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  class Todo extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      this.belongsTo(models.User, { foreignKey: 'user_id' })
-    }
-  }
-  Todo.init({
-    task: DataTypes.STRING,
-    text: DataTypes.STRING,
-    status: DataTypes.BOOLEAN,
-    user_id: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Todo',
-  });
-  return Todo;
-};
+	class Todo extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			this.belongsTo(models.User, { foreignKey: 'user_id' })
+		}
+	}
+	Todo.init(
+		{
+			task: DataTypes.STRING,
+			text: DataTypes.STRING,
+			status: DataTypes.BOOLEAN,
+			user_id: DataTypes.INTEGER,
+		},
+		{
+			sequelize,
+			modelName: 'Todo',
+		}
+	)
+	return Todo
+}
