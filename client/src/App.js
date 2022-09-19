9c94e183544f1c4b4f966cab9376d65b18a6d30e
andrey <elefant86@bk.ru>
2022-09-19 21:06:38 +0300

    add auth page & routes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Auth from './Components/Auth/Auth'
+import Register from './Components/Register/Register'
+import AddTask from './Components/AddTask/AddTask'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Routes>
+				<Route path='/' element={<Auth />} />
+				<Route path='/register' element={<Register />} />
+				<Route path='/add-task' element={<AddTask />} />
+			</Routes>
+		</div>
+	)
 }
 
-export default App;
+export default App
---
+++ client/src/Components/Auth/Auth.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Auth.css'
+
+function Auth() {
+	return (
+		<form name='authForm' method='post' action='input1.php'>
+			<div className='wrapper-auth'>
+				<div className='input-wrapper'>
+					<input className='text-input' type='text' name='name' />
+				</div>
+				<div className='input-wrapper'>
+					<input className='text-input' type='text' name='pass' />
+				</div>
+				<div className='input-wrapper'>
+					<div className='btn-wrapper'>
+						<div className='btn-wrapper-wrap'>
+							<input className='submit-btn' type='submit' value='Отправить' />
+						</div>
+						<div className='btn-wrapper-wrap'>
+							<Link to='/register'>
+								<input className='reg-btn' type='submit' value='Регистрация' />
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</form>
+	)
+}
+
+export default Auth
